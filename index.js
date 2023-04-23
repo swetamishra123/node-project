c773c5e6b7e4604535d96db98e8dda26b8f50560
swetamishra123 <swetamishra603@gmail.com>
2023-04-24 02:10:20 +0530

    Master

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+const  path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,7 +10,10 @@ const app= express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//set static folder
 
+
+app.use(express.static(path.join(_dirname , 'public')));
 app.use('/openai' , require('./routes/openaiRoutes'));
 
 app.listen(port , () => console.log(`Server started on port ${port}`));
